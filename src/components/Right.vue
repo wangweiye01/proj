2e37c30febc3286c01ea4284125dc08a6012377e
wangweiye <wwyknight@163.com>
2019-12-16 15:36:58 +0800

    fix

---
+++ src/components/Right.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-if="kk ==1">
+      <GoodList :goodId="1"></GoodList>
+    </div>
+
+    <div v-else-if="kk ==2">
+      <GoodList :goodId="2"></GoodList>
+    </div>
+
+    <div v-else>
+      <GoodList :goodId="0"></GoodList>
+    </div>
+  </div>
+</template>
+<script>
+import Msg from "./msg.js";
+import GoodList from "./goodList.vue";
+export default {
+  data() {
+    return {
+      kk: 0
+    };
+  },
+  mounted: function() {
+    var _this = this;
+    Msg.$on("fun", function(m) {
+      _this.kk = m;
+    });
+  },
+  components: {
+    GoodList
+  }
+};
+</script>
---
+++ src/components/goodList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div name="show">
+    <ul class="goodList">
+      <li v-for="good in list" v-bind:key="good.goodName">
+        <img v-bind:src="good.img" />
+        <p>{{good.goodName}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<style>
+.goodList li {
+  width: 200px;
+  height: 200px;
+  list-style: none;
+  float: left;
+  font-size: 9px;
+  color: red;
+  margin-bottom: 30px;
+}
+.goodList img {
+  width: 180px;
+  height: 180px;
+}
+</style>
+<script>
+export default {
+  name: "show",
+  data() {
+    var obj = this;
+    var url = "";
+    if (obj.goodId == 1) {
+      url = "json/bjb.json";
+    } else if (obj.goodId == 2) {
+      url = "json/sj.json";
+    } else {
+      url = "json/bjb.json";
+    }
+
+    this.$http.get(url).then(function(res) {
+      obj.list = res.data;
+    });
+    return {
+      list: []
+    };
+  },
+  props: {
+    goodId: Number
+  },
+  watch: {
+    goodId() {
+      var obj = this;
+      var url = "";
+      if (obj.goodId == 1) {
+        url = "json/bjb.json";
+      } else if (obj.goodId == 2) {
+        url = "json/sj.json";
+      } else {
+        url = "json/bjb.json";
+      }
+      this.$http.get(url).then(function(res) {
+        obj.list = res.data;
+      });
+      return {
+        list: []
+      };
+    }
+  }
+};
+</script>
