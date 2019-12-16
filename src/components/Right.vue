67a023e35e40f54807e4b767217c3db2c7d8d5e6
wangweiye <wwyknight@163.com>
2019-12-16 18:51:07 +0800

    fix

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
-    <div v-if="kk ==1">
-      <GoodList :goodId="1"></GoodList>
-    </div>
+    <div>
+        <div v-if="kk ==1">
+            <GoodList :goodId="1"></GoodList>
+        </div>
 
-    <div v-else-if="kk ==2">
-      <GoodList :goodId="2"></GoodList>
-    </div>
+        <div v-else-if="kk ==2">
+            <GoodList :goodId="2"></GoodList>
+        </div>
 
-    <div v-else>
-      <GoodList :goodId="0"></GoodList>
+        <div v-else>
+            <GoodList :goodId="0"></GoodList>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 import Msg from "./msg.js";
 import GoodList from "./goodList.vue";
 export default {
-  data() {
-    return {
-      kk: 0
-    };
-  },
-  mounted: function() {
-    var _this = this;
-    Msg.$on("fun", function(m) {
-      _this.kk = m;
-    });
-  },
-  components: {
-    GoodList
-  }
+    data() {
+        return {
+            kk: 0
+        };
+    },
+    mounted: function() {
+        var _this = this;
+        Msg.$on("fun", function(m) {
+            _this.kk = m;
+        });
+    },
+    components: {
+        GoodList
+    }
 };
 </script>
--- src/views/First.vue
+++ src/views/First.vue
@@ -1,47 +1,47 @@
 <template>
-  <div class="main">
-    <div class="left">
-      <Left></Left>
-    </div>
+    <div class="main">
+        <div class="left">
+            <Left></Left>
+        </div>
 
-    <div class="right">
-      <div class="top">
-        <img src="img/title.png" />
-      </div>
-      <div class="bottom">
-        <Right></Right>
-      </div>
+        <div class="right">
+            <div class="top">
+                <img src="img/title.png" />
+            </div>
+            <div class="bottom">
+                <Right></Right>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <style>
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 .left {
-  width: 100px;
-  float: left;
-  margin-right: 10px;
+    width: 100px;
+    float: left;
+    margin-right: 10px;
 }
 .right {
-  width: 1000px;
-  float: left;
-  margin-left: 10px;
+    width: 1000px;
+    float: left;
+    margin-left: 10px;
 }
 .main {
-  width: 1200px;
-  margin: 20px auto;
+    width: 1200px;
+    margin: 20px auto;
 }
 
 .top img {
-  height: 200px;
-  width: 1000px;
+    height: 200px;
+    width: 1000px;
 }
 .left,
 .right {
-  background-color: #f5f5f5;
-  height: 500px;
+    background-color: #f5f5f5;
+    height: 500px;
 }
 </style>
 <script>
@@ -49,9 +49,32 @@ import Left from "../components/Left.vue";
 import Right from "../components/Right.vue";
 
 export default {
-  components: {
-    Left,
-    Right
-  }
+    components: {
+        Left,
+        Right
+    }
 };
+
+// Promise的用法
+function test(resolve, reject) {
+    var timeOut = Math.random() * 2;
+    console.log("set timeout to: " + timeOut + " seconds.");
+    setTimeout(function() {
+        if (timeOut < 1) {
+            console.log("call resolve()...");
+            resolve("200 OK");
+        } else {
+            console.log("call reject()...");
+            reject("timeout in " + timeOut + " seconds.");
+        }
+    }, timeOut * 1000);
+}
+
+var p1 = new Promise(test);
+var p2 = p1.then(function(result) {
+    console.log("成功：" + result);
+});
+var p3 = p2.catch(function(reason) {
+    console.log("失败：" + reason);
+});
 </script>
